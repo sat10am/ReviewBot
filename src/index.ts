d530e0ebec740bc5eaa10c92ca2b7d8f411287c0
Hosung Lim <holnet1026@gmail.com>
2019-05-21 21:03:01 +0900

    feat: ⚡️ implement schedule notify

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,59 @@
-import { fetchPendingPullRequests, IPullRequest } from './lib/githubClient';
+import {
+  fetchPendingPullRequests,
+  IPullRequest,
+  IReviewRequest,
+} from './lib/githubClient';
+import { IncomingWebhook } from '@slack/webhook';
 import { config } from 'dotenv';
+import { scheduleJob } from "node-schedule";
 
 config();
 
-(async () => {
-  // const {
-  // 	repository: {
-  // 		pullRequests: {
-  // 			edges
-  // 		}
-  // 	}
-  // } = await fetchPendingPullRequests();
+const webhook = new IncomingWebhook(process.env.WEBHOOK_URL);
 
-  // console.log(edges);
+const notifyReviewRequest = async () => {
   const pullRequests: IPullRequest[] = await fetchPendingPullRequests();
+  const requests = pullRequests.map(v => {
+    const reviewers = v.node.reviewRequests.edges
+      .map((request: IReviewRequest) => request.node.requestedReviewer.name)
+      .join(', ');
+    return webhook.send({
+      attachments: [
+        {
+          color: '#439FE0',
+          fields: [
+            {
+              short: false,
+              title: 'URL',
+              value: v.node.url,
+            },
+            {
+              short: true,
+              title: 'Author',
+              value: v.node.author.login,
+            },
+            {
+              short: true,
+              title: 'Reviewer',
+              value: reviewers,
+            },
+          ],
+          title: v.node.title,
+          title_link: v.node.url,
+        },
+      ],
+      text: `${reviewers} 님 처리되지 않은 PR이 있어요!`,
+    });
+  });
 
-  console.log(JSON.stringify(pullRequests));
-})();
+  await Promise.all(requests);
+};
+
+process.env.NOTIFY_TIME.split(',').forEach((h) => {
+  const hour = Number(h);
+  scheduleJob({hour}, async () => {
+    await notifyReviewRequest();
+  });
+});
+
+
